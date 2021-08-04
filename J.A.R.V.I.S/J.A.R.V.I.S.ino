f710ed3b40725e9705a9c4650761be2fdf72fc6f
Steve <40924023+itsbravestone@users.noreply.github.com>
2021-08-04 23:58:15 +0530

    Update J.A.R.V.I.S.ino

--- J.A.R.V.I.S/J.A.R.V.I.S.ino
+++ J.A.R.V.I.S/J.A.R.V.I.S.ino
@@ -5,3 +5,21 @@
 
 #define SD_ChipSelectPin 53  
 TMRpcm audio;  
+
+VR myVR(10,11);
+uint8_t buf[255];
+
+void setup() {
+ myVR.begin(9600);
+ Serial.begin(115200);
+ audio.speakerPin = 46;
+ audio.setVolume(5);  
+ pinMode(13,OUTPUT);
+ if (!SD.begin(SD_ChipSelectPin)) {  
+    return;
+  }
+ else{
+    Serial.println("SD OK"); 
+  }
+}
+
