59544ee8faf786c44b4862c71b2a45773cfa4d78
Steve <40924023+itsbravestone@users.noreply.github.com>
2021-08-05 00:00:23 +0530

    Update vr_sample_train.ino

--- vr_sample_train/vr_sample_train.ino
+++ vr_sample_train/vr_sample_train.ino
@@ -29,3 +29,76 @@ int checkCMD(int len);
 int checkParaNum(int len);
 int findPara(int len, int paraNum, uint8_t **addr);
 int compareCMD(uint8_t *para1 , uint8_t *para2, int len);
+
+int cmdTrain(int len, int paraNum);
+int cmdLoad(int len, int paraNum);
+int cmdTest(int len, int paraNum);
+int cmdVR(int len, int paraNum);
+int cmdClear(int len, int paraNum);
+int cmdRecord(int len, int paraNum);
+int cmdSigTrain(int len, int paraNum);
+int cmdGetSig(int len, int paraNum);
+int cmdSettings(int len, int paraNum);
+int cmdHelp(int len, int paraNum);
+/** cmdList, cmdLen, cmdFunction has correspondence */
+const char cmdList[CMD_NUM][10] = {  // command list table
+  {
+    "train"  }
+  ,
+  {
+    "load"  }
+  , 
+  {
+    "clear"  }
+  ,
+  {
+    "vr"  }
+  ,
+  {
+    "record"  }
+  ,
+  {
+    "sigtrain"  }
+  ,
+  {
+    "getsig"  }
+  ,
+  {
+    "Settings"  }
+  ,
+  {
+    "test"  }
+  ,
+  {
+    "help"  }
+  ,
+};
+const char cmdLen[CMD_NUM]= {    // command length
+  5,  //  {"train"},
+  4,  //  {"load"}, 
+  5,  //  {"clear"},
+  2,  //  {"vr"},
+  6,  //  {"record"},
+  8,  //  {"sigtrain"},
+  6,  //  {"getsig"},
+  8,  //  {"Settings"},
+  4,  //  {"test"},
+  4,  //  {"help"}
+};
+cmd_function_t cmdFunction[CMD_NUM]={      // command handle fuction(function pointer table)
+  cmdTrain,
+  cmdLoad,
+  cmdClear,
+  cmdVR,
+  cmdRecord,
+  cmdSigTrain,
+  cmdGetSig,
+  cmdSettings,
+  cmdTest,
+  cmdHelp,
+};
+
+/***************************************************************************/
+/** temprory data */
+uint8_t buf[255];
+uint8_t records[7]; // save record
