9d6aa2b990796402e8a611952b7da0b02b5687f4
Steve <40924023+itsbravestone@users.noreply.github.com>
2021-08-04 23:59:39 +0530

    Update vr_sample_train.ino

--- vr_sample_train/vr_sample_train.ino
+++ vr_sample_train/vr_sample_train.ino
@@ -1,2 +1,19 @@
 #include <SoftwareSerial.h>
 #include "VoiceRecognitionV3.h"
+
+VR myVR(10,11);    // 2:RX 3:TX, you can choose your favourite pins.
+
+/***************************************************************************/
+/** declare print functions */
+void printSeperator();
+void printSignature(uint8_t *buf, int len);
+void printVR(uint8_t *buf);
+void printLoad(uint8_t *buf, uint8_t len);
+void printTrain(uint8_t *buf, uint8_t len);
+void printCheckRecognizer(uint8_t *buf);
+void printUserGroup(uint8_t *buf, int len);
+void printCheckRecord(uint8_t *buf, int num);
+void printCheckRecordAll(uint8_t *buf, int num);
+void printSigTrain(uint8_t *buf, uint8_t len);
+void printSystemSettings(uint8_t *buf, int len);
+void printHelp(void);
