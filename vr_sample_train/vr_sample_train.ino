acadbc6ee7afa6d0755444e557ab4d72dd133a64
Steve <40924023+itsbravestone@users.noreply.github.com>
2021-08-04 23:59:55 +0530

    Update vr_sample_train.ino

--- vr_sample_train/vr_sample_train.ino
+++ vr_sample_train/vr_sample_train.ino
@@ -17,3 +17,15 @@ void printCheckRecordAll(uint8_t *buf, int num);
 void printSigTrain(uint8_t *buf, uint8_t len);
 void printSystemSettings(uint8_t *buf, int len);
 void printHelp(void);
+
+#define CMD_BUF_LEN      64+1
+#define CMD_NUM     10
+typedef int (*cmd_function_t)(int, int);
+uint8_t cmd[CMD_BUF_LEN];
+uint8_t cmd_cnt;
+uint8_t *paraAddr;
+int receiveCMD();
+int checkCMD(int len);
+int checkParaNum(int len);
+int findPara(int len, int paraNum, uint8_t **addr);
+int compareCMD(uint8_t *para1 , uint8_t *para2, int len);
